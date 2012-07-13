bab39a82bb7f700575c8ec74111039b765975dad
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-13 07:54:32 +0700

    Initial commit

---
+++ lib/Net/HTTP/Methods/patch/log_request.pm
@@ -0,0 +1,72 @@
+package Net::HTTP::Methods::patch::log_request;
+
+use 5.010001;
+use strict;
+no warnings;
+
+use parent qw(Module::Patch);
+
+# VERSION
+
+our %config;
+
+my $p_log_request = sub {
+    require Log::Any;
+
+    my $orig = shift;
+    my $res = $orig->(@_);
+
+    my $log = Log::Any->get_logger;
+    $log->tracef("HTTP request: %s", $res);
+    $res;
+};
+
+sub patch_data {
+    return {
+        config => {
+        },
+        versions => {
+            # LWP is at 6.04, Net::HTTP 6.03, Net::HTTP::Methods still at 6.00
+            '6.00' => {
+                subs => {
+                    format_request => $p_log_request,
+                },
+            },
+        },
+    };
+}
+
+1;
+# ABSTRACT: Patch module for Net::HTTP::Methods
+
+=head1 SYNOPSIS
+
+ use Net::HTTP::Methods;
+ use Net::HTTP::Methods::patch::log_request
+   -on_unknown_version => 'warn',
+   -on_conflict        => 'warn';
+
+ # now all your LWP HTTP requests are logged
+
+ use LWP::UserAgent;
+ my $ua = LWP::UserAgent->new;
+ my $response = $ua->get('...');
+
+
+=head1 DESCRIPTION
+
+This module patches LWP::Protocol::http so that raw HTTP request is logged using
+L<Log::Any>. If you look into LWP::Protocol::http's source code, you'll see that
+it is already doing that (albeit commented):
+
+  my $req_buf = $socket->format_request($method, $fullpath, @h);
+  #print "------\n$req_buf\n------\n";
+
+
+=head1 FAQ
+
+=head2 Why not subclass?
+
+By patching, you do not need to replace all the client code which uses LWP.
+
+=cut

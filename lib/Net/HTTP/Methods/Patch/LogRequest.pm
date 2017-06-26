5ca28a531f6210061de8a508f037ce08110bfece
perlancar (on pc-home) <perlancar@gmail.com>
2017-06-26 11:56:04 +0700

    Switch from Log::Any to Log::ger

--- lib/Net/HTTP/Methods/Patch/LogRequest.pm
+++ lib/Net/HTTP/Methods/Patch/LogRequest.pm
@@ -13,8 +13,8 @@ use base qw(Module::Patch);
 our %config;
 
 my $p_format_request = sub {
-    require Log::Any::IfLOG;
-    my $log = Log::Any::IfLOG->get_logger;
+    require Log::ger;
+    my $log = Log::ger->get_logger;
 
     my $ctx = shift;
     my $orig = $ctx->{orig};
@@ -24,13 +24,15 @@ my $p_format_request = sub {
 
     if ($log->is_trace) {
 
-        # there is no equivalent of caller_depth in Log::Any, so we do this only
-        # for Log4perl
-        local $Log::Log4perl::caller_depth = $Log::Log4perl::caller_depth + 1
-            if $Log::{"Log4perl::"};
+        # XXX use equivalent for Log::ger
 
-        $log->tracef("HTTP request (proto=%s, len(headers)=%d):\n%s",
-                     $proto, length($res), $res);
+        # # there is no equivalent of caller_depth in Log::Any, so we do this only
+        # # for Log4perl
+        # local $Log::Log4perl::caller_depth = $Log::Log4perl::caller_depth + 1
+        #     if $Log::{"Log4perl::"};
+
+        $log->trace("HTTP request (proto=%s, len(headers)=%d):\n%s",
+                    $proto, length($res), $res);
     }
     $res;
 };
@@ -62,7 +64,7 @@ sub patch_data {
 
 Sample script and output:
 
- % LOG_SHOW_CATEGORY=1 TRACE=1 perl -MLog::Any::App \
+ % LOG_SHOW_CATEGORY=1 TRACE=1 perl -MLog::ger::Output=Screen \
    -MNet::HTTP::Methods::Patch::LogRequest -MWWW::Mechanize \
    -e'$mech=WWW::Mechanize->new; $mech->get("http://www.google.com/")'
  [cat Net.HTTP.Methods.Patch.LogRequest][23] HTTP request (142 bytes):
@@ -85,7 +87,7 @@ Sample script and output:
 =head1 DESCRIPTION
 
 This module patches Net::HTTP::Methods so that raw LWP HTTP request is logged
-using L<Log::Any>. If you look into LWP::Protocol::http's source code, you'll
+using L<Log::ger>. If you look into LWP::Protocol::http's source code, you'll
 see that it is already doing that (albeit commented):
 
   my $req_buf = $socket->format_request($method, $fullpath, @h);
